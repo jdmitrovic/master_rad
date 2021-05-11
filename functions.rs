5318b7692dbdce7e406a66878ae0dc1b71109e93
jovan <jdmitrovic@gmail.com>
2021-05-11 20:13:08 +0200

    Dodato do Vlasništva

---
+++ functions.rs
@@ -0,0 +1,3 @@
+fn zbir_kvadrata(x: i32, y: i32) -> i32 {
+  x*x + y*y
+}
---
+++ tuples_arrays.rs
@@ -0,0 +1,6 @@
+fn main() {
+   let koordinate: (i32, f32, i32) = (1, 2.0, 3);
+   let (x, y, z) = koordinate;
+
+   println!("x: {}, y: {}", x, koordinate.1);
+}
